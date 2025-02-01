e289272b58dab17f35564708c82af46d5eaf48ad
H-3D <himankchawla@outlook.com>
2025-02-01 17:26:34 +1300

    Add VGA text buffer support with printing functionality

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,16 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static BOOT_MESSAGE: &[u8] = b"booted the h-3x kernel successfully";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in BOOT_MESSAGE.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("booted the h-3x kernel successfully");
 
     loop {}
 }
